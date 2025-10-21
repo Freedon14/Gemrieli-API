1992561b9b72d8e4e1c8ff4ae135d5b654c04fdf
Freedon14 <5882126@gmail.com>
2025-10-22 01:31:16 +0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
-export default function handler(req, res) {
-  return res.status(200).json({ message: "Gemrieli API test ✅" });
+export default function handler(request, response) {
+  return response.status(200).json({ message: "Gemrieli API test ✅" });
 }
